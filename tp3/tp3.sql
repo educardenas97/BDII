477858cdeb27777eb2c4672b92662b647f2249c3
Eduardo Cardenas <eduardocardenas97@gmail.com>
2021-09-29 22:03:46 -0400

    ITEM C

--- tp3/tp3.sql
+++ tp3/tp3.sql
@@ -40,18 +40,19 @@ IS
         Verificar la fecha_operacion, la cual debe ser del año actual y anterior o igual a la
         fecha del sistema. No se admiten fechas adelantadas.
     */
-        IF extract(year from fecha_operacion) <= extract(year from SYSDATE) THEN
+        IF fecha_operacion <= SYSDATE THEN
         /*
             Verificar el código de operación, si el código de operación es de uso cajero:
         */
     
-            SELECT OP.cod_operacion into v_codigo_operacion FROM D_OPERACIONES OP WHERE USO_CAJERO = 1 AND OP.cod_operacion = cod_operacion;
+            SELECT OP.cod_operacion into v_codigo_operacion FROM 
+                D_OPERACIONES OP WHERE USO_CAJERO = 1 AND OP.cod_operacion = cod_operacion fetch first  rows only;
 
             IF v_codigo_operacion IS NOT NULL THEN
             /*
                 Debe verificar que el nro de caja sea not null y corresponda a una caja existente.
             */
-                    SELECT C.NRO_CAJA into v_numero_caja FROM D_CAJAS C WHERE C.NRO_CAJA = NRO_CAJA;
+                    SELECT C.NRO_CAJA into v_numero_caja FROM D_CAJAS C WHERE C.NRO_CAJA = NRO_CAJA FETCH FIRST ROWS ONLY;
 
                 IF v_numero_caja IS NOT NULL THEN
             /*
@@ -107,11 +108,12 @@ IS
                 
                 UPDATE D_TIPO_COMPROBANTE_SECUENCIA SET NRO_COMPROBANTE_ACTUAL = NRO_COMPROBANTE_ACTUAL + 1
                             WHERE COD_TIPO_COMPROBANTE = (
-                                SELECT TCS.COD_TIPO_COMPROBANTE 
-                                FROM D_TIPO_COMPROBANTE_SECUENCIA TCS 
-                                JOIN D_TIPO_COMPROBANTE TC ON TC.COD_TIPO_COMPROBANTE = TCS.COD_TIPO_COMPROBANTE
-                                JOIN D_MOVIMIENTO_OPERACIONES DMO ON TC.COD_TIPO_COMPROBANTE = DMO.COD_TIPO_COMPROBANTE
-                                WHERE DMO.cod_operacion = cod_operacion AND ANHO = extract(year from SYSDATE)
+                                SELECT TCS.COD_TIPO_COMPROBANTE
+                                    FROM D_TIPO_COMPROBANTE_SECUENCIA TCS
+                                    JOIN D_TIPO_COMPROBANTE TC ON TC.COD_TIPO_COMPROBANTE = TCS.COD_TIPO_COMPROBANTE
+                                    JOIN D_MOVIMIENTO_OPERACIONES DMO ON TC.COD_TIPO_COMPROBANTE = DMO.COD_TIPO_COMPROBANTE
+                                      WHERE DMO.cod_operacion = cod_operacion AND ANHO = extract(year from SYSDATE)
+                                
                             ) AND ANHO = (
                                 SELECT TCS.ANHO 
                                 FROM D_TIPO_COMPROBANTE_SECUENCIA TCS 
@@ -126,35 +128,76 @@ IS
                 V_DESC_OPERACION := descripcion_operacion;
             END IF;
             INSERT INTO D_MOVIMIENTO_OPERACIONES (
+                ID_OPERACION,
                 fecha_operacion, 
                 cod_sucursal, 
                 cod_operacion, 
                 id_persona, 
                 nro_caja,
                 id_usuario,
+                cod_tipo_comprobante,
+                NRO_COMPROBANTE,
                 TIPO_REGISTRO,
                 DESCRIPCION_OPERACION,
+                NRO_TIMBRADO,
                 FECHA_INSERT
             ) VALUES(
+                (SELECT MAX(ID_OPERACION)+1 FROM D_MOVIMIENTO_OPERACIONES),
                 fecha_operacion,
                 cod_sucursal,
                 cod_operacion,
                 id_persona,
                 nro_caja,
                 id_usuario,
+                1,
+                V_NRO_COMPROBANTE,
                 'A',
                 V_DESC_OPERACION,
+                V_NRO_TIMBRADO,
                 SYSDATE
             );
+
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('FECHA INCORRECTA');
         END IF;
         
     END;
-    
 
+/*
+c) El procedimiento P_ACTUALIZAR_STOCK que recibe como parámetros id_producto,
+cod_sucursal, cantidad, uso_stock
+El procedimiento debe actualizar la tabla D_STOCK_SUCURSAL, que almacena el stock de
+un producto en una sucursal, en base a los parámetros recibidos.
+• Si el valor del parámetro USO_STOCK es 2 (sumar), debe aumentar la
+CANTIDAD_EXISTENCIA en la tabla D_STOCK_SUCURSAL del producto dado por el
+ID_PRODUCTO, y en la sucursal determinada por el COD_SUCURSAL.
+• Si el valor del parámetro USO_STOCK es 1 (restar), debe disminuir la
+CANTIDAD_EXISTENCIA en la tabla D_STOCK_SUCURSAL del producto dado por el
+ID_PRODUCTO, y en la sucursal determinada por el COD_SUCURSAL. Ello siempre que la
+(CANTIDAD_EXISTENCIA - CANTIDAD_OPERACION) >= STOCK_MINIMO. Si no se cumple
+*/
 
-    
-                
-                   
+CREATE OR REPLACE PROCEDURE P_ACTUALIZAR_STOCK(
+v_id_producto IN D_STOCK_SUCURSAL.ID_PRODUCTO%TYPE,
+v_cod_sucursal IN D_STOCK_SUCURSAL.COD_SUCURSAL%TYPE, 
+v_cantidad IN D_STOCK_SUCURSAL.CANTIDAD_EXISTENCIA%TYPE,
+v_uso_stock NUMBER
+)
+IS
+    BEGIN
+        IF V_USO_STOCK = 2 THEN
+            UPDATE D_STOCK_SUCURSAL SET CANTIDAD_EXISTENCIA = CANTIDAD_EXISTENCIA + V_CANTIDAD
+            WHERE ID_PRODUCTO = V_ID_PRODUCTO 
+            AND COD_SUCURSAL = v_cod_sucursal;
+        ELSIF V_USO_STOCK = 1 THEN
+            UPDATE D_STOCK_SUCURSAL SET CANTIDAD_EXISTENCIA = CANTIDAD_EXISTENCIA - V_CANTIDAD
+            WHERE ID_PRODUCTO = V_ID_PRODUCTO 
+            AND COD_SUCURSAL = v_cod_sucursal   
+            AND (CANTIDAD_EXISTENCIA - V_CANTIDAD) >= STOCK_MINIMO; 
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('***USO STOCK NO VALIDO***');
+        END IF;
+    END;
 
 
 
