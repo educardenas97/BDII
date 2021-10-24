79435a61beb8774be21a4e0a450e1b9ee25220bc
Eduardo Cardenas <eduardocardenas97@gmail.com>
2021-10-24 18:21:38 -0300

    item 1 b_c

--- tp4/item1_b_c.sql
+++ tp4/item1_b_c.sql
@@ -1,3 +1,6 @@
+CREATE OR REPLACE TRIGGER item1
+    AFTER INSERT ON D_PAGO_OPERACION
+    FOR EACH ROW
 DECLARE
     -- Tablas a recorrer
     TYPE detalle_operaciones IS TABLE OF D_DETALLE_OPERACIONES%ROWTYPE;
@@ -17,7 +20,7 @@ DECLARE
     consulta_productos VARCHAR(500);
     consulta_detalles VARCHAR(400);
     consulta_porcentajes VARCHAR(400);
-
+    consulta_update VARCHAR(400);
     -- Input variables
     v_id_pago_operacion NUMBER;
     v_id_forma_pago NUMBER;
@@ -32,13 +35,23 @@ DECLARE
     importe_recargo NUMBER;
     importe_operacion NUMBER;
     importe_iva NUMBER;
+
+    -- Sumatoria
+    sum_importe_operacion NUMBER;
+    sum_importe_descuento NUMBER;
+    sum_importe_recargo NUMBER;
 BEGIN
     -- test
-    v_id_pago_operacion := 2;
-    v_id_forma_pago := 1;
+    v_id_pago_operacion := :NEW.id_operacion;
+    v_id_forma_pago := :NEW.cod_forma_pago;
     ---------
 
-    
+    --
+    sum_importe_operacion := 0;
+    sum_importe_descuento := 0;
+    sum_importe_recargo := 0;
+    --
+
     -- Obtener detalle de operaciones
     -- parametros:
     --   id_operacion
@@ -111,7 +124,6 @@ BEGIN
                 --b.5
                 importe_iva := (importe_operacion - importe_descuento + importe_recargo) / (divisor(divisor.FIRST).divisor_iva_incluido);
 
-
                 DBMS_OUTPUT.PUT_LINE('Producto_medida_precio------');
                 DBMS_OUTPUT.PUT_LINE('--- Precio: '||v_productos_medida_precio(j).precio_venta);
                 DBMS_OUTPUT.PUT_LINE('--- Importe descuento: '||importe_descuento);
@@ -119,11 +131,35 @@ BEGIN
                 DBMS_OUTPUT.PUT_LINE('--- Importe operacion: '||importe_operacion);
                 DBMS_OUTPUT.PUT_LINE('--- Importe iva: '||importe_iva);
                 DBMS_OUTPUT.PUT_LINE('---------------------------');
-                DBMS_OUTPUT.PUT_LINE(' ');
                 
+                -- Sumatoria
+                sum_importe_operacion := sum_importe_operacion + importe_operacion;
+                sum_importe_descuento := sum_importe_descuento + importe_descuento;
+                sum_importe_recargo := sum_importe_recargo + importe_recargo;
 
                 j:= v_productos_medida_precio.NEXT(j);
             END LOOP; 
+
+            IF sum_importe_operacion <> 0 THEN
+                -- mostrar sumatoria
+                DBMS_OUTPUT.PUT_LINE('Sumatoria');
+                DBMS_OUTPUT.PUT_LINE('--- Importe recargo: '||sum_importe_recargo);
+                DBMS_OUTPUT.PUT_LINE('--- Importe operacion: '||sum_importe_operacion);
+                DBMS_OUTPUT.PUT_LINE('--- Importe descuento: '||sum_importe_descuento);
+                -- reset sumatoria
+                sum_importe_operacion := 0;
+                sum_importe_descuento := 0;
+                sum_importe_recargo := 0;
+                DBMS_OUTPUT.PUT_LINE('---------------------------');
+                DBMS_OUTPUT.PUT_LINE(' ');
+                -- update importe_operacion en la tabla d_pago_operacion
+                consulta_update := 'UPDATE D_PAGO_OPERACION
+                SET IMPORTE_PAGO = :1
+                WHERE ID_OPERACION = :2';
+                EXECUTE IMMEDIATE consulta_update
+                USING sum_importe_operacion, v_detalle_operaciones(i).id_operacion;
+            END IF;
+
             i:= v_detalle_operaciones.NEXT(i);
         END LOOP;
 END;
